e166414cc2bdb5a618b96b4f91c567406dc27f4a
Tyler Brown <118109189+tylerbeckb@users.noreply.github.com>
2023-01-10 19:30:26 +0000

    Added comments and changed the dictionaries name

--- main_pipeline.py
+++ main_pipeline.py
@@ -1,5 +1,4 @@
 import json
-import csv
 from csv import DictReader
 
 def retrieve (data_dict, variety, min_score, max_score, region ) -> list:
@@ -16,20 +15,23 @@ def retrieve (data_dict, variety, min_score, max_score, region ) -> list:
                     json_list.append(items)
     return json_list
 
-def save_json(json_list):
+def save_json(json_list) -> None:
+    # Opens the json file to write too
     output = open('output.json', 'w')
+    # Converts the list into a string
     json_string = json.dumps(json_list)
+    # Writes it to the file
     output.write(json_string)
+    # Closes the file
+    output.close()
 
 # Creates list variable
-header = []
-data = []
 json_data = []
 # Opens csv file
 with open('wine_data.csv', 'r') as file:
     # Adds all the data to a dictionary
     dict_reader = DictReader(file)
-    list_of_dict = list(dict_reader)
+    list_of_data = list(dict_reader)
 
 # Asks for user inputs for each condition
 wine_variety = input( 'Enter the wine type to find: ' )
@@ -38,5 +40,6 @@ max_rating = int(input( 'Enter an integer for the maximum wine score to find: ' 
 wine_region = input( 'Enter the region of wine to find: ' )
 
 # Calls function to retrieve the data that meets the conditions
-json_data = retrieve(list_of_dict, wine_variety, min_rating, max_rating, wine_region)
+json_data = retrieve(list_of_data, wine_variety, min_rating, max_rating, wine_region)
+# Calls the function that writes data to a json file
 save_json(json_data)
