2cdeaa4e08374fbbb4028ff07dccdf91679c25e8
Tyler Brown <118109189+tylerbeckb@users.noreply.github.com>
2023-01-10 19:26:59 +0000

    Changed the retrieve function to use the dictionary and converted rating to a float and then into a string

--- main_pipeline.py
+++ main_pipeline.py
@@ -2,16 +2,16 @@ import json
 import csv
 from csv import DictReader
 
-def retrieve (data_list, variety, min_score, max_score, region ) -> list:
+def retrieve (data_dict, variety, min_score, max_score, region ) -> list:
     json_list = []
     # Iterates through all the data
-    for rows in range(len(data_list)):
+    for items in data_dict:
         # Checks if the type matches
-        if items[3] == variety:
+        if items['variety'] == variety:
             # Checks if the region matches
-            if items[2] == region:
+            if items['region'] == region:
                 # Checks if the rating is in the range
-                if items[4] >= min_score & items[4] <= max_score:
+                if int(float(items['rating'])) >= min_score & int(float(items['rating'])) <= max_score:
                     # Adds the data to the list
                     json_list.append(items)
     return json_list
@@ -29,7 +29,6 @@ json_data = []
 with open('wine_data.csv', 'r') as file:
     # Adds all the data to a dictionary
     dict_reader = DictReader(file)
-     
     list_of_dict = list(dict_reader)
 
 # Asks for user inputs for each condition
@@ -39,5 +38,5 @@ max_rating = int(input( 'Enter an integer for the maximum wine score to find: ' 
 wine_region = input( 'Enter the region of wine to find: ' )
 
 # Calls function to retrieve the data that meets the conditions
-json_data = retrieve(data, wine_variety, min_rating, max_rating, wine_region)
+json_data = retrieve(list_of_dict, wine_variety, min_rating, max_rating, wine_region)
 save_json(json_data)
