6a2e33cdbeaf02222abae431b8e95fe752166345
Tyler Brown <118109189+tylerbeckb@users.noreply.github.com>
2023-01-10 17:30:44 +0000

    Added comments and changed print statement layouts to include ':'

--- main_pipeline.py
+++ main_pipeline.py
@@ -15,7 +15,7 @@ def retrieve (data_list, variety, min_score, max_score, region ) -> list:
                     json_list.append(items)
     return json_list
 
-def save_json():
+def save_json(json_list):
     pass
 
 # Creates list variable
@@ -30,9 +30,12 @@ with open('wine_data.csv') as file:
     for row in file:
         data.append ( row )
 
+# Asks for user inputs for each condition
 wine_variety = input( 'Enter the wine type to find' )
-min_rating = int(input( 'Enter an integer for the minimum wine score to find' ))
-max_rating = int(input( 'Enter an integer for the maximum wine score to find' ))
-wine_region = input( 'Enter the region of wine to find' )
+min_rating = int(input( 'Enter an integer for the minimum wine score to find: ' ))
+max_rating = int(input( 'Enter an integer for the maximum wine score to find: ' ))
+wine_region = input( 'Enter the region of wine to find: ' )
 
+# Calls function to retrieve the data that meets the conditions
 json_data = retrieve(data, wine_variety, min_rating, max_rating, wine_region)
+save_json(json_data)
