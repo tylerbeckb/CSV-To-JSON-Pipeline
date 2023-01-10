efe0f5f9500b7e904ab063127e14a90eee39977d
Tyler Brown <118109189+tylerbeckb@users.noreply.github.com>
2023-01-10 17:00:51 +0000

    Gets the users inputs for the conditions of wine to find and put in the JSON file

--- main_pipeline.py
+++ main_pipeline.py
@@ -1,7 +1,7 @@
 import json
 import csv
 
-def retrieve ( header ,data, type, score_range, region ):
+def retrieve ( header ,data, type, min_score, max_score, region ):
     pass
 
 def save_json():
@@ -10,11 +10,18 @@ def save_json():
 # Creates list variable
 header = []
 data = []
+json_data = []
 # Opens csv file
-with open( 'wine_data.csv') as file:
+with open('wine_data.csv') as file:
     # Reads the headers
     header = next ( file )
     # Adds all the data to a list
     for row in file:
         data.append ( row )
 
+wine_type = input( 'Enter the wine type to find' )
+min_score = int(input( 'Enter an integer for the minimum wine score to find' ))
+max_score = int(input( 'Enter an integer for the maximum wine score to find' ))
+region = input( 'Enter the region of wine to find' )
+
+json_data = retrieve(header, data, wine_type, min_score, max_score, region)
